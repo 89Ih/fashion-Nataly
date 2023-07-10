476e551fa56c4b082cfafe1373cfdf85319334b0
89Ih <ehabhamza94@gmail.com>
2023-07-10 20:42:52 +0200

    push -u origin main

--- components/header.js
+++ components/header.js
@@ -7,7 +7,7 @@ class Header extends HTMLElement {
 
       <!-- Logo  -->
 
-      <a href="/">
+      <a href="/fashion-Nataly/">
         <img height="50px" width="50px" src="https://res.cloudinary.com/dpcxthabw/image/upload/v1688304021/Nataly/logo_s9ma3x.png" alt="Nataly-logo" />
         </a>
 
